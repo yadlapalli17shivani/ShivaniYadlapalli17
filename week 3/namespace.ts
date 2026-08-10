59370b5409fb91f7a808c831a13758b6f8182e8b
Shivani Yadlapalli <yadlapallishivani17@gmail.com>
2026-08-10 18:59:39 +0530

    Add files via upload

---
+++ week 3/namespace.ts
@@ -0,0 +1,19 @@
+namespace MathOperations {
+    export function add(a: number, b: number) {
+        return a + b;
+    }
+
+    export function subtract(a: number, b: number) {
+        return a - b;
+    }
+
+    export function multiply(a: number, b: number) {
+        return a * b;
+    }
+
+    export function divide(a: number, b: number) {
+        return a / b;
+    }
+}
+
+console.log(MathOperations.add(10, 5));
