0cdb64dc9888d7b5abc6e6b54f34ac4fe2e11299
Shivani Yadlapalli <yadlapallishivani17@gmail.com>
2026-09-19 09:37:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h2>HTML Rendering in React</h2>
+
+      <div>
+        <h3 style={{ color: "blue" }}>Welcome to React</h3>
+
+        <p>This HTML content is rendered using JSX.</p>
+
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
